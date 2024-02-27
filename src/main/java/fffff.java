c535de14594a16424249fd126bb80114061790d5
Johnny <sconner2230@gmail.com>
2024-02-27 21:28:06 +0300

    dgfdgdfgdfgdfdf

--- src/main/java/fffff.java
+++ src/main/java/fffff.java
@@ -3,6 +3,6 @@
  */
 public class fffff {
     public static void main(String[] args) {
-        System.out.println("Hello qorld!!!");
+        System.out.println("Hello wwwwwwwworld!!!");
     }
 }
